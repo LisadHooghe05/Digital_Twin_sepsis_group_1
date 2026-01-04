25309cd83a4e3a55462a826f049725826a40c873
adamstrv <adam@oink-oink-oink.net>
2026-01-04 17:38:30 +0100

    terugzetten main

--- main.py
+++ main.py
@@ -23,38 +23,38 @@ OUTPUT_CSV = "AKI_stage_output.csv"
 OUTPUT_CSV2 = "AKI_subjects.csv"
 
 def main():
-    #AKI_df = AKI_detection(SEPSIS_CSV,CREAT_CSV)
-    #AKI_df.to_csv(OUTPUT_CSV)
-    #print(f"Total amount of patients with sepsis: {AKI_df['subject_id'].nunique()}")
-    #AKI_subjects_df = (AKI_df.loc[AKI_df["AKI"], ["subject_id"]].drop_duplicates().assign(AKI_stage="AKI").reset_index(drop=True))
-    #AKI_subjects_df.to_csv(OUTPUT_CSV2)
-    #print(f"AKI output saved as: {OUTPUT_CSV}")
-    #print(f"AKI subjects saved as: {OUTPUT_CSV2}")
-    #matrix = build_feature_matrix()
-    #print(f"Amount of patients in matrix: {matrix['subject_id'].nunique()}")
-    #missing_values,medians_df,final_matrix = fill_matrix_with_zeros()
+    AKI_df = AKI_detection(SEPSIS_CSV,CREAT_CSV)
+    AKI_df.to_csv(OUTPUT_CSV)
+    print(f"Total amount of patients with sepsis: {AKI_df['subject_id'].nunique()}")
+    AKI_subjects_df = (AKI_df.loc[AKI_df["AKI"], ["subject_id"]].drop_duplicates().assign(AKI_stage="AKI").reset_index(drop=True))
+    AKI_subjects_df.to_csv(OUTPUT_CSV2)
+    print(f"AKI output saved as: {OUTPUT_CSV}")
+    print(f"AKI subjects saved as: {OUTPUT_CSV2}")
+    matrix = build_feature_matrix()
+    print(f"Amount of patients in matrix: {matrix['subject_id'].nunique()}")
+    missing_values,medians_df,final_matrix = fill_matrix_with_zeros()
     # Print missing values greater than 0
-    #print("Missing values in some columns:")
-    #print(missing_values[missing_values['missing_values'] > 0])
+    print("Missing values in some columns:")
+    print(missing_values[missing_values['missing_values'] > 0])
 
     # Print which medians were used for filling
-    #print("Medians used for vitals:")
-    #print(medians_df)
+    print("Medians used for vitals:")
+    print(medians_df)
 
     # The final matrix
-    #matrix_path = REPO_ROOT / "matrix_filled.csv"
-    #final_matrix.to_csv(matrix_path, index=False)
+    matrix_path = REPO_ROOT / "matrix_filled.csv"
+    final_matrix.to_csv(matrix_path, index=False)
 
     # Making dashboard CSVs and save them in the folder csv_dashboard
-    #print("Generating creatinine CSV...")
-    #df_creatinine = build_creatinine_window_12h_aki()
-    #print("Generating heart rate CSV...")
-    #df_heartrate = build_heartrate_12h_before_aki()
-    #print("Generating medical history CSV...")
-    #df_conditions = save_conditions_per_subject()
-    #print("Generating medication CSVs...")
-    #df_med_binary, df_med_amounts = combine_all_to_one_csv_long_with_time()
-    #print("All dashboard CSVs generated successfully!")
+    print("Generating creatinine CSV...")
+    df_creatinine = build_creatinine_window_12h_aki()
+    print("Generating heart rate CSV...")
+    df_heartrate = build_heartrate_12h_before_aki()
+    print("Generating medical history CSV...")
+    df_conditions = save_conditions_per_subject()
+    print("Generating medication CSVs...")
+    df_med_binary, df_med_amounts = combine_all_to_one_csv_long_with_time()
+    print("All dashboard CSVs generated successfully!")
 
     
     print("Running unsupervised clustering...")
